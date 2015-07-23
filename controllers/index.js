3d1002633841912cd47d6512476b7d2834a2382e
Steve Domenico <stratcat944@gmail.com>
2015-07-23 15:12:53 -0600

    first commit

---
+++ controllers/index.js
@@ -0,0 +1,17 @@
+var food = require('../models/food.js')
+
+var indexController = {
+	index: function(req, res) {
+		// var allFoods = food.allFoods;
+		res.render('index', {foodieFoods : food.allFoods});
+	},
+
+	foodPage : function(req, res){
+		var foodSlug = req.params.slug;
+		var foundFood = food.foodFind(foodSlug);
+
+		res.render('food', {food : foundFood});
+	}
+};
+
+module.exports = indexController;
---
+++ models/food.js
@@ -0,0 +1,47 @@
+// List of all foodItem instances
+var allFoods = [];
+
+// This will be our base class for our model
+var FoodItem = function(name, flavor, image){
+	this.name = name;
+	// Bananas Foster - won't work, two words.
+	this.slug = name.trim().toLowerCase().replace(/ /g, '-');
+	this.flavor = flavor;
+	this.image = image;
+
+}
+// Bananas Foster
+
+var addFood = function(name, flavor, image){
+	var newFood = new FoodItem(name, flavor, image)
+	allFoods.push(newFood)
+}
+
+addFood('Pizza', 'Savory', 'http://slice.seriouseats.com/images/20071024pizzerialamozzer-thumb.jpg');
+
+addFood('Cake', 'Delicious', 'http://static.guim.co.uk/sys-images/music/Pix/pictures/2009/5/22/1242985956697/Morrissey-with-birthday-c-002.jpg');
+
+addFood('Turkey', 'Fowl', 'http://pixel.nymag.com/imgs/daily/grub/2015/02/03/03-morrissey-turkey.w750.h560.2x.jpg');
+
+var foodFind = function(slug){
+	return allFoods.filter(function(element){
+		return element.slug === slug
+	})[0]
+}
+
+var foodDelete= function(slug){
+
+	allFoods = allFoods.filter(function(element){
+		return element.slug !== slug
+	})
+
+}
+
+
+module.exports = {
+	allFoods 	: allFoods,
+	addFood 	: addFood,
+	FoodItem 	: FoodItem,
+	foodFind 	: foodFind,
+	foodDelete 	: foodDelete
+}
